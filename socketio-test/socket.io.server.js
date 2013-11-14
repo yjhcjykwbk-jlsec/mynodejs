832c886d4b1e9b9b0f67f5fbed6d773ea217970f
Administrator <x@ubuntu.(none)>
2013-11-14 22:58:34 +0800

    ..

--- socketio-test/socket.io.server.js
+++ socketio-test/socket.io.server.js
@@ -7,20 +7,19 @@ var server= http.createServer(function(req, res){
   res.writeHead(200,{ 'Content-Type': 'text/html' }); 
   res.end('<h1>Hello Socket Lover!</h1>'); 
 }); 
-server.listen(8080); 
+server.listen(8888); 
 
 // 创建一个Socket.IO实例，把它传递给服务器
 var socket= io.listen(server); 
-
 // 添加一个连接监听器
+var i=0;
 socket.on('connection', function(client){ 
-
   // 成功！现在开始监听接收到的消息
-  client.on('message',function(event){ 
-    console.log('Received message from client!',event); 
+  client.on('message',function(msg){ 
+    console.log('Received message from client:',msg); 
+    client.send("[msg "+(i++)+"]hello, i am server!");
   }); 
   client.on('disconnect',function(){ 
-    clearInterval(interval); 
     console.log('Server has disconnected'); 
   }); 
 });
